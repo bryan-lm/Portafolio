d8253348d301557e77cfab7d0a94cf2ea9206169
bryan luz <bryanluzmartinez@gmail.com>
2023-11-12 23:43:49 -0600

    fin de la seccion

--- src/app/pages/abaut/abaut.component.ts
+++ src/app/pages/abaut/abaut.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { InfoPaginaService } from '../../services/info-pagina.service';
 
 @Component({
   selector: 'app-abaut',
   templateUrl: './abaut.component.html',
   styleUrls: ['./abaut.component.css']
 })
-export class AbautComponent {
+export class AbautComponent implements OnInit{
+  constructor(public infoService:InfoPaginaService){}
+  ngOnInit(){
+      
+  }
 
 }
--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -9,18 +9,37 @@ export class InfoPaginaService {
 
   info:InfoPagina={};
   cargada=false;
+  equipo:any[]=[];
 
   constructor(private http:HttpClient) {
-    //console.log('servicio de infopagina listo');
+    
+    this.cargarInfo();
+    this.cargarEquipo();
+
+
+
+   }
+
+   private cargarInfo(){
     this.http.get('assets/data/data-pagina.json')
-        .subscribe((resp:InfoPagina)=>{
-          this.cargada=true;
-          this.info=resp;
-          console.log(resp);
-          
+    .subscribe((resp:InfoPagina)=>{
+      this.cargada=true;
+      this.info=resp;
+      console.log(resp);
+      
 
-        });
+    });
+   }
+   private cargarEquipo(){
 
+    this.http.get('https://angular-html-25e99-default-rtdb.firebaseio.com/equipo.json')
+    .subscribe((resp:any)=>{
+      this.equipo=resp;
+    
+      //console.log(resp);
+      
+
+    });
 
    }
 }
